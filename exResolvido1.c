e549fcfdec189da0ac9019827a5656521e9cd207
FernandoCMP <79115755+FernandoCMP@users.noreply.github.com>
2021-08-20 13:56:02 -0300

    Create exResolvido1.c
    Faça um programa que leia dois números inteiros e depois os imprima na ordem inversa em que eles foram lidos.

---
+++ exResolvido1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+  int v1,v2; //declaração das variáveis
+  scanf("%d%d",&v1,&v2); //leitura dos valores
+  printf("%d e %d",v2,v1); //impressão invertida dos valores
+  return 0; // Se tudo ocorrer corretamente, o programa retornará zero 
+}
