58e7e5a72e9d81c620ceba30c5861298f79ccc41
Profº Esp. Pedro Fernando M. Cabral <79115755+FernandoCMP@users.noreply.github.com>
2021-08-24 15:43:25 -0300

    Update exConstantes.c

--- exConstantes.c
+++ exConstantes.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#define PI 3.141592 //Constantes são definidas antes da função main
+#define PI 3.141592 //Constantes são escritas em caixa alta e definidas antes da função main
 int main() {
     printf("O valor de PI é: %f", PI );
 }
