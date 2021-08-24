7bffa51eeda01bde18651bac3d1d256f748abb74
Profº Esp. Pedro Fernando M. Cabral <79115755+FernandoCMP@users.noreply.github.com>
2021-08-24 16:05:50 -0300

    Create exEscape.c
    Exemplo: Sequências de Escape

---
+++ exEscape.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+  printf("Sequências\n de\n Escape\n\n");//Quebra de Linha
+  printf("Sequências\t de\t Escape\n\n");//Tabulação Horizontal
+  printf("Sequências\v de\v Escape\n\n");//Tabulação Vertical
+  printf("Sequências de Escape\b \n\n");//Backspace
+  printf("Sequências de Escape\a"); //Som de alerta
+  return 0;
+}
