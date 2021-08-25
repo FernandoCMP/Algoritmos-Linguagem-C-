fde65213bd21af69ed37619a8175bb18c4eb7457
Profº Esp. Pedro Fernando M. Cabral <79115755+FernandoCMP@users.noreply.github.com>
2021-08-25 10:57:27 -0300

    Create exConstantes.c

---
+++ aula03/exConstantes.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#define PI 3.141592 //Constantes são escritas em caixa alta e definidas antes da função main
+int main() {
+    printf("O valor de PI é: %f", PI );
+    return 0;
+}
