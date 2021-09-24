5366c06567a4e22b9f9d413d7c0d5ec3104b8d1c
Profº Esp. Pedro Fernando M. Cabral <79115755+FernandoCMP@users.noreply.github.com>
2021-09-24 13:55:19 -0300

    Create exFor.c

---
+++ aula05/exFor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int a,b,c;
+	printf("Digite o valor de a: ");
+	scanf("%d",&a);
+	printf("Digite o valor de b: ");
+	scanf("%d",&b);
+	for (c = a; c <= b; c++){
+		printf("%d \n",c);
+	}
+	return 0;
+}
